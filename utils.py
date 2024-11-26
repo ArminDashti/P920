0a2b9719e28082958d6979fc157b89613636855a
ArminDashti <arminonline71@gmail.com>
2024-11-26 23:27:52 +0330

    Update

--- utils.py
+++ utils.py
@@ -89,6 +89,7 @@ def normalize_rewards(reward_batch):
     return (reward_batch - mean_reward) / (std_reward + 1e-8)
 
 
+
 def soft_update(source_model, target_model, tau):
     for source_param, target_param in zip(source_model.parameters(), target_model.parameters()):
         target_param.data.copy_(tau * source_param.data + (1 - tau) * target_param.data)
